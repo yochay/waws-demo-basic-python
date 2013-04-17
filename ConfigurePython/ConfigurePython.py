a64ece701c5b7c3c54c2147cb8f4a94e59a457c3
unknown <yochayk@YOCHAYWIN8_2.redmond.corp.microsoft.com>
2013-04-17 06:24:36 -0700

    change output format into html

--- ConfigurePython/ConfigurePython.py
+++ ConfigurePython/ConfigurePython.py
@@ -2,4 +2,7 @@ def application(environ, start_response):
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
-    yield 'Hello from Windows Azure Websites\n'
+	output = '<html>'
+	output += '<h1>This is a demo for basic python running on WAWS</h1>'
+	output += '</html>'
+	return [output]

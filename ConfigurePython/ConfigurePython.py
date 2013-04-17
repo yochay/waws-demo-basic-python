eaff771e3019c27266cc8dcc0b364a0d2dc6bf44
unknown <yochayk@YOCHAYWIN8_2.redmond.corp.microsoft.com>
2013-04-17 06:30:09 -0700

    change output format into html update output var

--- ConfigurePython/ConfigurePython.py
+++ ConfigurePython/ConfigurePython.py
@@ -1,8 +1,8 @@
 def application(environ, start_response):
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain')]
-    start_response(status, response_headers)
 	output = '<html>'
 	output += '<h1>This is a demo for basic python running on WAWS</h1>'
 	output += '</html>'
-	return [output]
+	start_response('200 OK', [('Content-type', 'text/html')])
+	return output
